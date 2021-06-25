e1a4e02e5b7676596e92342b60a0c273652e5d78
nand <nand12398@gmail.com>
2021-06-26 00:09:25 +0530

    Comic Page(Second Page) Initial commit with Basic Html only

---
+++ comic.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Random Comic Book</title>
+</head>
+<body>
+	<div id="topcontainer">
+		&nbsp;
+	</div>
+	<div id="container">
+		<div id="banner">
+			<h1> <a href="./">Welcome to Comic World</a></h1>
+		</div>
+	</div>
+	<div id="container2">
+		<div id="left">
+			<div id="nav">
+				<ul>
+					<li><a href="./"><span>Home</span></a></li>
+					<li><a href="./comic.php"><span>Comic</span></a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="content">
+			<h1>Random Comic</h1>
+		</div>
+
+		<div>
+		</div>
+
+		<div id="footer">
+			<form name="subscribe" action="" method="POST">
+				<input type="text" name="email_id" placeholder="Enter Your Email ID" required>&nbsp;&nbsp;&nbsp;
+				<button type="submit" name="submit"> Subscribe </button>
+			</form>
+			<p><a rel="license" href="http://creativecommons.org/licenses/by-nc/2.5/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/2.5/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/2.5/">Creative Commons Attribution-NonCommercial 2.5 Generic License</a>.</p>
+		</div>
+	</div>
+</body>
+</html>
