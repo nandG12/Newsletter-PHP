343b042a143d32a5b77d8e90d465820a2a3789ad
nand <nand12398@gmail.com>
2021-06-26 00:08:16 +0530

    Home Page Initial commit with basic html only

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Comic World</title>
+</head>
+<body>
+	<div id="topcontainer">
+		&nbsp;
+	</div>
+	<div id="container">
+		<div id="banner">
+			<h1> <a href="./">Welcome to Comic World</a></h1>
+		</div>
+	</div>
+	<div id="container2">
+		<div id="left">
+			<div id="nav">
+				<ul>
+					<li><a href="./"><span>Home</span></a></li>
+					<li><a href="./comic.php"><span>Comic</span></a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="content">
+			<h1>What is Comic Book?</h1>
+			<p>A magazine that presents a serialized story in the form of a comic strip, typically featuring the adventures of a superhero.</p>
+			<h1>Why are they called Comics?</h1>
+			<p>They were called comics or "funnies" because the were, for the most part, comical stories meant for light entertainment. The first "comic books" were collected versions of comic strips that appeared in newspapers.</p>
+		</div>
+
+		<div>
+		</div>
+
+		<div id="footer">
+			<form name="subscribe" action="" method="POST">
+				<input type="text" name="email_id" placeholder="Enter Your Email ID" required>&nbsp;&nbsp;&nbsp;
+				<button type="submit" name="submit"> Subscribe </button>
+			</form>
+			<p><a rel="license" href="http://creativecommons.org/licenses/by-nc/2.5/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/2.5/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc/2.5/">Creative Commons Attribution-NonCommercial 2.5 Generic License</a>.</p>
+		</div>
+	</div>
+</body>
+</html>
