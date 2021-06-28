e2b27b2588a1536e419aca3bb15c4026c745c64b
nand <nand12398@gmail.com>
2021-06-28 10:38:42 +0530

    JavaScript Related Function, Email Verification

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function validateEmail()  {  
+  var x=document.subscribe.email_id.value;  
+  var atposition=x.indexOf("@");  
+  var dotposition=x.lastIndexOf(".");  
+  if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length) {  
+    alert("Please enter a valid e-mail address");  
+    return false;  
+  }  
+}
