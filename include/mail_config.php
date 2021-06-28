c0c61f2063798451ad762ab10423c38d0c8de1a6
nand <nand12398@gmail.com>
2021-06-28 12:03:02 +0530

    PHP Mailer - Mail Configuration

---
+++ include/mail_config.php
@@ -0,0 +1,22 @@
+<?php 
+	//Import the PHP Mailer Library
+	require '../lib/phpmailer/PHPMailerAutoload.php';
+
+	$mail = new PHPMailer;
+	$mail->isSMTP();
+
+	//Change the SMTP Configuration as per your mail provider. Below is an example of the Google Mail SMTP Configuration.
+	$mail->Host='smtp.gmail.com';
+	$mail->Port=587;
+	$mail->SMTPAuth=true;
+	$mail->SMTPSecure='TLS';
+
+	//Enter Your E-Mail Username & Password
+	$mail->Username='';
+	$mail->Password='';
+
+	//Enter the E-Mail that you are using
+	$mail->setFrom('xxx@xxx.com');
+	$mail->addReplyTo('xxx@xxx.com');
+
+?>
